f1f775ae9afaf89409db3b86313e5b5fa561abc3
Ramiro <ariasramirox@gmail.com>
2020-04-04 20:10:45 -0300

    Correcciones de formato

--- Ruleta.py
+++ Ruleta.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
 import random
 
@@ -6,85 +6,93 @@ import random
 class Constant:
     TIRADAS = 10000
     CANTMONTECARLO = 36
-#Valor del desvio del .... ???
-colors = ['red','blue','green','yellow','purple']
-color = ['r','b','g','y','p']
-#Valor de la varianza del numero X(6) con respecto a N
-#varianza6 = []
+
+
+colors = ['red', 'blue', 'green', 'yellow', 'purple']
+color = ['r', 'b', 'g', 'y', 'p']
+
+
 def getInput():
     correct = False
     while correct != True:
-        inputNumber = int(input("Dígame un número: "))
-        if inputNumber >= 0 and inputNumber <=  Constant.CANTMONTECARLO :
+        inputNumber = int(input("Dígame un número de la ruleta (entre 0 y 36):"))
+        if inputNumber >= 0 and inputNumber <= Constant.CANTMONTECARLO:
             correct = True
-        else: 
+        else:
             print('Numero Fuera de indice, Elija entre 0 y 36')
     else:
         return inputNumber
+
 
 def getValorPromedio(tirada):
     c = 0
     promedio = 0
     valorPromedio = []
-    for i in tirada: 
-        c+=1
+    for i in tirada:
+        c += 1
         promedio = promedio + i
         valorPromedio.append(promedio/c)
     return valorPromedio
 
-def getFrecuenciaRelativa(tirada,input):
+
+def getFrecuenciaRelativa(tirada, input):
     frecRelativaNum = []
     absoluteFrecuency = 0
     c = 0
     for i in tirada:
         c = c + 1
-        if i == input :    
-            absoluteFrecuency = absoluteFrecuency + 1 
-        frecRelativaNum.append(absoluteFrecuency/c)            
+        if i == input:
+            absoluteFrecuency = absoluteFrecuency + 1
+        frecRelativaNum.append(absoluteFrecuency/c)
     return frecRelativaNum
 
-def getTirada(quantity,maxValue):
+
+def getTirada(quantity, maxValue):
     tirada = []
-    for i in range(1,quantity):
-        #Obtener valor random dentro de el rango y añadirlo a la lista
-        tirada.append(random.randint(0,maxValue))
+    for i in range(1, quantity):
+        # Obtener valor random dentro de el rango y añadirlo a la lista
+        tirada.append(random.randint(0, maxValue))
     return tirada
+
 
 def getVarianza(tirada):
     varianzaArr = []
     c = 1
-    tiradaLenght =  len(tirada)
+    tiradaLenght = len(tirada)
     while c < tiradaLenght:
         varianzaArr.append(np.var(tirada[0:c]))
-        c +=1
+        c += 1
     return varianzaArr
+
+
 def getDesviacion(tirada):
     return np.sqrt(getVarianza(tirada))
+
 ################ MAIN LOOP ################
 inputNumber = getInput()
-tirada = getTirada(Constant.TIRADAS,Constant.CANTMONTECARLO)
+tirada = getTirada(Constant.TIRADAS, Constant.CANTMONTECARLO)
 
-#Grafica frecuencia relativa
-plt.plot(getFrecuenciaRelativa(tirada,inputNumber))
+# Grafica frecuencia relativa
+plt.plot(getFrecuenciaRelativa(tirada, inputNumber))
 plt.show()
 
-#Grafica valor promedio
+# Grafica valor promedio
 plt.figure()
-plt.plot([-100,Constant.TIRADAS],[18,18],'r-o')
-plt.plot( getValorPromedio(tirada)  )
-plt.annotate('Valor promedio esperado',color = 'red', xy = (Constant.TIRADAS/2, 17.95), xytext = (Constant.TIRADAS/2, 19),
-             arrowprops=dict(facecolor = 'red',edgecolor = 'red', shrink = 0.05),
+plt.plot([-100, Constant.TIRADAS], [18, 18], 'r-o')
+plt.plot(getValorPromedio(tirada))
+plt.annotate('Valor promedio esperado',color='red', xy=(Constant.TIRADAS/2, 17.95), xytext=(Constant.TIRADAS/2, 19),
+             arrowprops=dict(facecolor='red', edgecolor='red', shrink=0.05),
              )
 plt.show()
 
-#Grafica varianza
+# Grafica varianza
 plt.figure()
-plt.plot(getVarianza(tirada)) 
+plt.plot(getVarianza(tirada))
 plt.show()
-#Grafica desviacion
+# Grafica desviacion
 plt.figure()
-plt.plot(getDesviacion(tirada)) 
+plt.plot(getDesviacion(tirada))
 plt.show()
-#Lista completa de tiradas
-#for i in (lista):
+# Lista completa de tiradas
+# for i in (lista):
 #    print (lista[i])
