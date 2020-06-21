de592d52ef39eaab82b69ad89e114e4300acf298
Franco Pinacca <francoo_27@hotmail.com>
2020-06-21 15:33:24 -0300

    Cambios en las variables de la clase simulacion , por falta de conocimiento estabamos creando variables de clase en vez de instancia lo que hacia que al crear una nueva instancia de simulacion se mantengan valores; Fix de error de resultados de simulacion , en a parte de calcular el evento de salida en el metodo departure  se dividia por 1/midTimeArrivals en vez de  1/midTimeService.

--- MM1.py
+++ MM1.py
@@ -17,27 +17,7 @@ class EVENT_TYPE(Enum):
 
 # Sitema MM1
 class Sim:
-    id=0
-    nextEvent = "" # A - Arribo | P - Partida
-    serverEstatus = 0 # 1 - Diponible | 0 - Ocupado
-    numberOfClientsInQueue = 0
-    timeServiceacumulated = 0.0
-    clock = 0.0
-    midTimeArrivals = 0.0
-    midTimeService = 0.0
-    numberOfClientsCompletedWithDelay = 0
-    timeOfLastEvent = 0.0
-    eventList = []
-    queue = []
-    totalDelay = 0.0
-    areaQ = 0.0
-    areaB = 0.0
-    simulationEnded = False
-    #Listas para generar las gráficas
-    clientQueueinT = []
-    clockinT = []
-    tsAcuminT = []
-    def __init__(self,id,serverEstatus,
+    def __init__(self,codigo,serverEstatus,
                 nextEvent,clock,
                 totalDelay,timeServiceacumulated,
                 areaQ,areaB,
@@ -46,23 +26,28 @@ class Sim:
                 number,
                 midTimeArrivals,
                 midTimeService): 
-        self.id=id
-        self.serverEstatus = serverEstatus
-        self.nextEvent = nextEvent
-        self.clock = clock
-        self.totalDelay = totalDelay
-        self.timeServiceacumulated = timeServiceacumulated
-        self.areaQ = areaQ
-        self.areaB = areaB
-        self.timeOfLastEvent = timeOfLastEvent
-        self.numberOfClientsInQueue = numberOfClientsInQueue
-        self.numberOfClientsCompletedWithDelay = numberOfClientsCompletedWithDelay
-        self.midTimeArrivals = midTimeArrivals
-        self.midTimeService = midTimeService
-
+        self.codigo=codigo or 0
+        self.serverEstatus = serverEstatus or SERVER_STATUS.DISPONIBLE.value
+        self.nextEvent = nextEvent or ""
+        self.clock = clock or 0.0
+        self.totalDelay = totalDelay or 0.0
+        self.timeServiceacumulated = timeServiceacumulated or 0.0
+        self.areaQ = areaQ or 0.0
+        self.areaB = areaB or 0.0
+        self.timeOfLastEvent = timeOfLastEvent or 0.0 
+        self.numberOfClientsInQueue = numberOfClientsInQueue or 0
+        self.numberOfClientsCompletedWithDelay = numberOfClientsCompletedWithDelay or 0.0
+        self.midTimeArrivals = midTimeArrivals or 0.0
+        self.midTimeService = midTimeService or 0.0
+        self.eventList = []
+        self.queue = []
+        #Listas para generar las gráficas
+        self.clientQueueinT = []
+        self.clockinT = []
+        self.tsAcuminT = []
         #Tiempo del primer arribo
         self.eventList.append(np.random.exponential(1/self.midTimeArrivals))
-
+        self.simulationEnded = False
         #Le pongo un número grande para asegurarme que el primer evento sea un arribo
         self.eventList.append(number)
 
@@ -99,8 +84,6 @@ class Sim:
             self.tsAcuminT.append(self.timeServiceacumulated)
             
         else:
-            ####### A ESTE ELSE NO ENTRA NUNCA #######
-
             #Calculo área bajo Q(t) desde el momento actual del reloj hacia atrás (tiempo del último evento)
             self.areaQ += (self.numberOfClientsInQueue * (self.clock - self.timeOfLastEvent))     
 
@@ -119,11 +102,9 @@ class Sim:
     
     def departure(self):          
         #Pregunto si hay clientes en cola 
-
-        ####### A LA PRIMERA PARTE DEL IF NO ENTRA NUNCA #######
         if self.numberOfClientsInQueue > 0:
             #Tiempo del próximo evento partida
-            self.eventList[1] = self.clock + np.random.exponential(1/self.midTimeArrivals)
+            self.eventList[1] = self.clock + np.random.exponential(1/self.midTimeService)
             #Acumulo la la demora acumulada como el valor actual del reloj menos el valor del
             # reloj cuando el cliente ingresó a la cola
             self.totalDelay += self.clock - self.queue[0]
@@ -188,7 +169,16 @@ class Sim:
         else:
             average_costumers_delay = 0
             return average_costumers_delay
-        
+def inputNumber(message):
+  while True:
+    try:
+       userInput = int(input(message))       
+    except ValueError:
+       print("No es un entero , porfavor ingrese nuevamente")
+       continue
+    else:
+       return userInput 
+       break      
     
 def main(simulacion):
     while ( (not simulacion.simulationEnded) or (simulacion.numberOfClientsInQueue != 0 ) or (simulacion.serverEstatus != SERVER_STATUS.DISPONIBLE.value)):
@@ -201,29 +191,19 @@ def main(simulacion):
             simulacion.simulationEnded = True
             break
 
-    #     pprint(vars(simulacion))
-    #     print("eventList",simulacion.eventList)
-    #     print("queue",simulacion.queue)
-    #     print("clientQueueinT",simulacion.clientQueueinT)
-    #     print("clockinT",simulacion.clockinT)
-    #     print("tsAcuminT",simulacion.tsAcuminT)
-    # print('Cantidad promedio de clientes en cola:', simulacion.getMeanOfClientsInQueue())
-    # print('Promedio de utilizacion del servidor:', simulacion.getMeanOfServerUtilization())        
-    # print('Tiempo promedio de demora de los clientes:', simulacion.getAverageCustomerDelay())    
-
 
 def runSimulations(count):
     i=0
     while i < count:
-        simulacion = Sim(i,SERVER_STATUS.DISPONIBLE.value,EVENT_TYPE.UNKNOWN.value,0.0,0.0,0.0,0.0,0.0,0.0,0,0,99999999,7.0,9.0)
+        a=i
+        simulacion = Sim(a,SERVER_STATUS.DISPONIBLE.value,EVENT_TYPE.UNKNOWN.value,0.0,0.0,0.0,0.0,0.0,0.0,0,0,99999999,7.0,9.0)
         main(simulacion)
         simulaciones.append(simulacion)
         i+=1
     for sim in simulaciones:
-        # print(pprint(vars(sim)))
         print('Cantidad promedio de clientes en cola:', sim.getMeanOfClientsInQueue())
         print('Promedio de utilizacion del servidor:', sim.getMeanOfServerUtilization())        
         print('Tiempo promedio de demora de los clientes:', sim.getAverageCustomerDelay())
+    
 
-
-runSimulations(1)
+runSimulations(inputNumber("Ingrese el numero de simulaciones: "))
